2ac2958be99b20af5eafbe8790b070cf3d0cb51b
Drag Simeonov <mi.e.drago@gmail.com>
2023-12-16 17:32:10 +0200

    Update firebaseConfig.js

--- src/appWorkers/firebaseConfig.js
+++ src/appWorkers/firebaseConfig.js
@@ -1,11 +1,5 @@
 export const firebaseConfig  = {
-    apiKey: "AIzaSyBrJA-A-KL1QzUa1MRbgLSIcDEw_5Wyu7Y",
-    authDomain: "music-intervals-test-react.firebaseapp.com",
-    databaseURL: "https://music-intervals-test-react.firebaseio.com",
-    projectId: "music-intervals-test-react",
-    storageBucket: "music-intervals-test-react.appspot.com",
-    messagingSenderId: "75447750336",
-    appId: "1:75447750336:web:6cc8fc1479d28a11"
+   
   };
   
   
